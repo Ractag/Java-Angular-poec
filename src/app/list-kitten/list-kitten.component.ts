bf778449523805d63631d165ac2f6b3224c45578
Victor G <victorgj95@outlook.com>
2024-04-21 11:36:57 +0200

    created components and form

---
+++ src/app/list-kitten/list-kitten.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Kitten } from '../models/kitten.model';
+
+@Component({
+  selector: 'app-list-kitten',
+  templateUrl: './list-kitten.component.html',
+  styleUrl: './list-kitten.component.css',
+})
+export class ListKittenComponent {
+  kittenList: Array<Kitten> = [];
+}
